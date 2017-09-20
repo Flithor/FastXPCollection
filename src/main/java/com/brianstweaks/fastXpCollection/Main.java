e9c56e0200733a3c8fb1c816036b2aaebbfad477
brian91292 <8050523+brian91292@users.noreply.github.com>
2017-09-20 15:07:30 -0500

    Update Main.java
    Update version

--- src/main/java/com/brianstweaks/fastXpCollection/Main.java
+++ src/main/java/com/brianstweaks/fastXpCollection/Main.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class Main
 {
     public static final String MODID = "fastxpcollection";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     public static SimpleNetworkWrapper network;
     
