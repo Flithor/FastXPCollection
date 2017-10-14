475ecc42bdd201afbf51b176919f668584135768
Flithor <flithor@live.com>
2017-10-14 18:51:51 +0800

    Update Main.java
    fit 1.12.X

--- src/main/java/com/brianstweaks/fastXpCollection/Main.java
+++ src/main/java/com/brianstweaks/fastXpCollection/Main.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
-@Mod(modid = Main.MODID, version = Main.VERSION)
+@Mod(modid = Main.MODID, version = Main.VERSION, acceptedMinecraftVersions="[1.12]")
 public class Main
 {
     public static final String MODID = "fastxpcollection";
